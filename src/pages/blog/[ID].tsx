c3fb065223b73aab3249c8dc2b291545e9a3e8b6
halkichi0308 <m.kouda0308@gmail.com>
2023-07-24 03:58:47 +0900

    fix each blog content.

--- src/pages/blog/[ID].tsx
+++ src/pages/blog/[ID].tsx
@@ -5,14 +5,17 @@ type Props = {
     blog: Blog[];
 };
 
-const BlogId: React.FC<Props> = ({ blog }: any) => {
+const BlogId: React.FC<Props> = (blogs) => {
+    let blog: Blog = blogs.blog[0];
+    let elem: Element;
     return (
         <main>
             <h1>{blog.title}</h1>
             <p>{blog.publishedAt}</p>
+            <p>test</p>
             <div
                 dangerouslySetInnerHTML={{
-                    __html: `${blog.content}`,
+                    __html: `${blog.title}`,
                 }}
             />
         </main>
@@ -30,13 +33,12 @@ export const getStaticPaths = async () => {
     return { paths, fallback: false };
 };
 
-export const getStaticProps = async (context: { params: { id: string } }) => {
-    const id = context.params.id;
-    const data = await client.get({ endpoint: "blog", contentId: id });
+export const getStaticProps = async () => {
+    const data = await client.get({ endpoint: "blog" });
 
     return {
         props: {
-            blog: data,
+            blog: data.contents,
         },
     };
 };
